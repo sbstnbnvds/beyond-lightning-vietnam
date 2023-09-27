99174f090f3262d9172bbdc7db1d37ebaa5ad056
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-09-27 14:41:38 -0500

    navbar 1st commit

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -4,14 +4,16 @@ const TopSection = () => {
   return (
     <div className="top-section">
       <div className="navbar">
-        <button>Botón de Hamburguesa</button>
-        <button>Buy Tickets Now</button>
+        <div className="menu-btn">
+          <div className='hamburger-menu' />
+        </div>
+        <button className='buy-tickets'>Buy Tickets</button>
       </div>
 
       <div className="title-section">
         <h1>Beyond Lightning Vietnam</h1>
         <div className="title-info">
-          <h2>Danang, Vietnam</h2>
+          <h2>Da Nang, Vietnam</h2>
           <h2>March 29-30, 2024</h2>
         </div>
       </div>
