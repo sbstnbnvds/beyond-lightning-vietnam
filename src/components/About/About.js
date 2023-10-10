f087351a43c5871a5eb5271d6b2311d69adda411
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-10-10 16:54:37 -0500

    about section secondary layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,8 @@
-import "./About.css"
+import "./About.css";
+import leftArrow from "../../assets/SliderArrowLeft.svg";
+import rightArrow from "../../assets/SliderArrowRight.svg";
+import calendar from "../../assets/CalendarIcon.png"
+import location from "../../assets/LocationIcon.png"
 
 const About = () => {
   return (
@@ -6,30 +10,30 @@ const About = () => {
       <div className="section-wrapper">
         <div className="about-section__main">
           <div className="about-section__main__title-wrapper">
-          <h2 className="about-section__main-title"><span className="break-span">The only </span> Bitcoin & Lightning Conference <span className="secondary-span"> in Vietnam</span></h2>
+            <h2 className="about-section__main-title"><span className="break-span">The only </span> Bitcoin & Lightning Conference <span className="secondary-span"> in Vietnam</span></h2>
 
           </div>
           <div>
-            <p>Welcome to the official website of Beyond Lightning Vietnam, powered by company name, by NeutronPay.</p> 
+            <p>Welcome to the official website of Beyond Lightning Vietnam, powered by company name, by NeutronPay.</p>
             <p>The premier conference focusing on the future of Bitcoin, the Lightning Network and more! This is your exclusive invitation to be part of a circle of innovators steering the course of future developments.</p>
           </div>
         </div>
-        <div className="about-section__secondary">
-          <div className="about-section__secondary__info">
-            <h3>Conference</h3>
-            <div>
-              <div>
-                <div><img src="https://picsum.photos/50" alt="" aria-hidden="true"></img></div>
+        <div className="about-section__secondary flex-column">
+          <div className="about-section__secondary__info flex-column">
+            <h3><span className="break-span">About the </span>Conference</h3>
+            <div className="about-section__secondary__info__text flex">
+              <div className="flex">
+                <div className="icon-wrapper"><img src={calendar} alt="" aria-hidden="true"></img></div>
                 <p><span>When: </span>March 29-30, 2024.</p>
               </div>
-              <div>
-                <div></div><img src="https://picsum.photos/50" alt="" aria-hidden="true" />
+              <div className="flex">
+                <div className="icon-wrapper"><img src={location} alt="" aria-hidden="true" /></div>
                 <p><span>Where: </span> Furama Resort, Da Nang, Vietnam.</p>
               </div>
             </div>
           </div>
-          <div>
-            <img src="https://picsum.photos/200" alt="Video" />
+          <div className="video-wrapper">
+            <img src="https://picsum.photos/840/600" alt="Video" />
           </div>
         </div>
         <div className="slider">Slider</div>
