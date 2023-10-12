d3f1bf8bd9ce0645c60828419b1847ef609b2fe5
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-10-11 19:58:52 -0500

    media partners responsive section

--- src/components/MediaPartners/MediaPartners.js
+++ src/components/MediaPartners/MediaPartners.js
@@ -1,9 +1,20 @@
+import "./MediaPartners.css"
+
+import BitcoinMagazine from "../../assets/bitcoin-magazine-vector-logo_blue 1.svg";
+import TheDailyHodl from "../../assets/TheDailyHodlLogo.svg";
+import BeInCrypto from "../../assets/BeInCryptoLogo.svg";
+
+
 const MediaPartners = () => {
   return (
     <section className="media-partners-section bg-secondary icons-section">
       <div className="section-wrapper">
-        <h2>Partners</h2>
-        <div></div>
+        <h2><span>Media </span>Partners</h2>
+        <div className="icons-list flex">
+          <div className="icon-wrapper"><img src={BitcoinMagazine} alt="Bitcoin Magazine Logo" /></div>
+          <div className="icon-wrapper"><img src={TheDailyHodl} alt="The Daily Hodl Logo" /></div>
+          <div className="icon-wrapper"><img src={BeInCrypto} alt="Be In Crypto Logo" /></div>
+        </div>
       </div>
     </section>
   )
