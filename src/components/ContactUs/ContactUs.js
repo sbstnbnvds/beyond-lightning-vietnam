fa822363001f1e0c579152a46226c9a4513d91fb
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-10-27 19:09:22 -0500

    Contact Us section

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,9 +1,22 @@
+import "./ContactUs.css"
+
 import React from 'react'
 
+
 const ContactUs = () => {
+
   return (
-    <section className="contact-us-section bg-primary-2">
-      Contact Us Section.
+    <section className="contact-us-section bg-primary-2 flex-column">
+      <h2>Contact Us</h2>
+
+      <form className="flex-column contact-us-form" action="">
+        <input type="text" name="name" placeholder="Name*" onChange={() => console.log("form input")} />
+        <input type="text" name="email" placeholder="Email*" onChange={() => console.log("form input")}/>
+        <input type="text" name="company" placeholder="Company*" onChange={() => console.log("form input")}/>
+        <input type="text" name="reason" placeholder="Reason for contact*" onChange={() => console.log("form input")}/>
+        <input type="text" name="additional" placeholder="Additional info (optional)" onChange={() => console.log("form input")}/>
+        <button>Submit</button>
+      </form>
     </section>
   )
 }
