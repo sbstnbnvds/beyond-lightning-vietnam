a29e8d515f3c2f17081edf718987f77618a40e34
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-10-27 19:09:34 -0500

    side events animation setup

--- src/components/SideEvents/SideEvents.js
+++ src/components/SideEvents/SideEvents.js
@@ -20,47 +20,93 @@ const SideEvents = () => {
         <li className="">Conference</li>
         <li className="">BTC gaming</li>
       </ul>
-      <div className="main-div flex">
-        <div className="img-wrapper wrapper-1 flex-column">
-          <div className="img-container">
-            <img src={Hackathon1} alt="" />
+      <div className="main-div-wrapper">
+        <div className="main-div not-active flex">
+          <div className="img-wrapper wrapper-1 flex-column">
+            <div className="img-container">
+              <img src={Hackathon1} alt="" />
+            </div>
+            <div className="img-container alt-bg">
+              <img src={Hackathon2} alt="" />
+              <div></div>
+            </div>
+            <div className="img-container mobile-only">
+              <img src={Hackathon4} alt="" />
+            </div>
+            <div className="img-container mobile-only alt-bg">
+              <img src={Hackathon3} alt="" />
+              <div></div>
+            </div>
           </div>
-          <div className="img-container alt-bg">
-            <img src={Hackathon2} alt="" />
-            <div></div>
+          <div className="card flex-column">
+            <p className="date">March 29</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt est similique eos a, ab quasi adipisci maiores molestias!</p>
+            <div className="heading">
+              <h3><span className="span-break">1 - Day </span>Hackathon</h3>
+            </div>
+
           </div>
-          <div className="img-container mobile-only">
-            <img src={Hackathon4} alt="" />
+          <div className="img-wrapper wrapper-2 flex-column">
+            <div className="img-container alt-bg">
+              <img src={Hackathon3} alt="" />
+              <div></div>
+            </div>
+            <div className="img-container">
+              <img src={Hackathon4} alt="" />
+            </div>
           </div>
-          <div className="img-container mobile-only alt-bg">
-            <img src={Hackathon3} alt="" />
-            <div></div>
+          <div className="img-wrapper wrapper-3 flex-column">
+            <div className="img-container">
+              <img src={Hackathon5} alt="" />
+            </div>
+            <div className="img-container alt-bg">
+              <img src={Hackathon6} alt="" />
+              <div></div>
+            </div>
           </div>
         </div>
-        <div className="card flex-column">
-          <p className="date">March 29</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt est similique eos a, ab quasi adipisci maiores molestias!</p>
-          <div className="heading">
-            <h3><span className="span-break">1 - Day </span>Hackathon</h3>
+        <div className="main-div flex">
+          <div className="img-wrapper wrapper-1 flex-column">
+            <div className="img-container">
+              <img src={Hackathon1} alt="" />
+            </div>
+            <div className="img-container alt-bg">
+              <img src={Hackathon2} alt="" />
+              <div></div>
+            </div>
+            <div className="img-container mobile-only">
+              <img src={Hackathon4} alt="" />
+            </div>
+            <div className="img-container mobile-only alt-bg">
+              <img src={Hackathon3} alt="" />
+              <div></div>
+            </div>
           </div>
-        
-        </div>
-        <div className="img-wrapper wrapper-2 flex-column">
-          <div className="img-container alt-bg">
-            <img src={Hackathon3} alt="" />
-            <div></div>
+          <div className="card flex-column">
+            <p className="date">March 29</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt est similique eos a, ab quasi adipisci maiores molestias!</p>
+            <div className="heading">
+              <h3><span className="span-break">1 - Day </span>Hackathon</h3>
+            </div>
+
           </div>
-          <div className="img-container">
-            <img src={Hackathon4} alt="" />
+          <div className="img-wrapper wrapper-2 flex-column">
+            <div className="img-container alt-bg">
+              <img src={Hackathon3} alt="" />
+              <div></div>
+            </div>
+            <div className="img-container">
+              <img src={Hackathon4} alt="" />
+            </div>
           </div>
-        </div>
-        <div className="img-wrapper wrapper-3 flex-column">
-          <div className="img-container">
-            <img src={Hackathon5} alt="" />
-          </div>
-          <div className="img-container alt-bg">
-            <img src={Hackathon6} alt="" />
-            <div></div>
+          <div className="img-wrapper wrapper-3 flex-column">
+            <div className="img-container">
+              <img src={Hackathon5} alt="" />
+            </div>
+            <div className="img-container alt-bg">
+              <img src={Hackathon6} alt="" />
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
