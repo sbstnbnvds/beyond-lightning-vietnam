eeac084183161b9fe6c5b7fb5d53fdffc720fa45
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-11-07 14:41:51 -0500

    sidebar 1st commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./Navbar.css"
+import "./Sidebar.css"
 import { Link } from "react-router-dom"
 
 const Navbar = ({ page }) => {
@@ -10,6 +11,8 @@ const Navbar = ({ page }) => {
     setClick(!click);
     console.log("event")
   }
+
+
   return (
     <>
     <nav className={page === 'ticketing' ? 'navbar ticketing' : 'navbar'}>
@@ -18,12 +21,36 @@ const Navbar = ({ page }) => {
           <span className='hamburger-menu' />
         </button>
       </div>
-      <Link to={"ticketing"} >
-        <button className='buy-tickets'>Buy Tickets</button>
+      <Link to={"ticketing"} className='buy-tickets' >
+        Buy Tickets
       </Link>
     </nav>
-    <aside className={click ? 'active-sidebar' : ''}>
-      dsdfs
+
+
+    <aside className={(click ? 'active-sidebar' : '') + (page === 'ticketing' ? ' sidebar-ticketing' : '')}>
+      <ul className="pages-list">
+        <li className="active-page">
+          <strong>ActivePage</strong>
+          <ul className="sections-list">
+            <li>Section1</li>
+            <li>Section2</li>
+            <li>Section3</li>
+            <li>Section4</li>
+            <li>Section5</li>
+            <li>Section6</li>
+            <li>Section7</li>
+            <li>Section8</li>
+            <li>Section9</li>
+            <li>Section10</li>
+            <li>Section11</li>
+            <li>Section12</li>
+            <li>Section13</li>
+          </ul>
+        </li>
+        <li>
+          <strong>TicketingPage</strong>
+        </li>
+      </ul>
     </aside>    
     </>
   )
